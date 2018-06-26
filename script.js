e4bfce54067390c61f58407bc9db85df93ca8662
Diyon2019 <40601282+Diyon2019@users.noreply.github.com>
2018-06-26 12:31:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-<script>
 $(document).ready(function(){
-  $(button).click(function(){
+  $("button").click(function(){
    alert("Hello from my page");
-});
-
-</script>
+  $("iframe").hide();
+  });
+ });
